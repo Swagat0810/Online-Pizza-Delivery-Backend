fea705a4446aee0df850575a87df60341f7cc05a
Swagat0810 <82587453+Swagat0810@users.noreply.github.com>
2022-08-29 13:59:08 +0530

    Add files via upload

---
+++ src/main/java/com/cg/onlinepizzaapp/onlinepizzaapp/controller/AdminController.java
@@ -0,0 +1,59 @@
+package com.cg.onlinepizzaapp.onlinepizzaapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.onlinepizzaapp.onlinepizzaapp.entity.Admin;
+import com.cg.onlinepizzaapp.onlinepizzaapp.service.IAdminService;
+
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/admins")
+@Validated
+	public class AdminController {
+		@Autowired
+		 IAdminService adminService;
+		/*
+		 * login
+		 */
+		@GetMapping("/signIn/{adminName}/{adminPassword}")
+		public Admin signIn(@PathVariable String adminName,@PathVariable String adminPassword) {
+			return adminService.signIn(adminName,adminPassword);
+			
+			}
+
+		/*
+		 * adding user
+		 */
+		@PostMapping("/addNewAdmin")
+		public ResponseEntity<Admin> add(@RequestBody Admin admin) {
+			Admin admin1 = adminService.addNewAdmin(admin);
+			return new ResponseEntity<>(admin1, HttpStatus.OK);
+		}
+		/*
+		 * signOut user
+		 */
+		
+		@PostMapping(value="/signOut")
+		public void signOutAdmin(@RequestBody Admin admin){
+			adminService.signOut(admin);
+		
+	}
+		
+		
+}
+
+
+
+
+
